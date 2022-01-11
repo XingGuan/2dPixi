adc8a7163b392ef96477ee38f7667d05527816e4
13756613348 <1456725324@qq.com>
2022-01-11 20:07:48 +0800

    补充

--- src/index.js
+++ src/index.js
@@ -14,7 +14,9 @@ PIXI.utils.sayHello(type);
 // import Icon from './asset/img/bg.png';  
 
 let Application = PIXI.Application,
-    Sprite = PIXI.Sprite;
+    Sprite = PIXI.Sprite,
+    TextureCache = PIXI.utils.TextureCache,
+    Rectangle = PIXI.Rectangle;
 
 let loader = new PIXI.Loader(),
     resources = loader.resources;
@@ -37,7 +39,8 @@ scaleToWindow(app.view, '#2C3539');
 
 loader
     .add("./asset/img/figure.png")
-    .add("./asset/img/figure1.png")
+    .add("./asset/img/tileset.png")
+    .add("./asset/img/main.json")
     .load(setup);
 
 // 在整个过程中，可以发出多个信号。
@@ -73,9 +76,65 @@ function loadProgressHandler() {
 //这个setup方法将在图片加载完成后执行
 function setup() {
     console.info('setup');
-    //创建人物这个精灵
-    let figure = new Sprite(resources["./asset/img/figure.png"].texture);
-    //添加人物到舞台
-    app.stage.addChild(figure);
+    // //创建人物这个精灵
+    // let figure = new Sprite(resources["./asset/img/figure.png"].texture);
+    // //定位精灵
+    // figure.position.set(50, 50);
+    // //设置大小
+    // figure.width = 120;
+    // figure.height = 80;
+    // //改变精灵锚点
+    // figure.anchor.set(0.5, 0.5);
+
+    // //添加人物到舞台
+    // app.stage.addChild(figure);
+
+    //从纹理创建`tileset`精灵
+    // let texture = TextureCache["./asset/img/tileset.png"];
+    // let rectangle = new Rectangle(96, 64, 32, 32);
+    // //告诉纹理使用那个矩形截面 
+    // texture.frame = rectangle;
+    // //从纹理创建精灵   
+    // let rocket = new Sprite(texture);
+    // rocket.x = 32;
+    // rocket.y = 32;
+    // app.stage.addChild(rocket);
+    // app.renderer.render(app.stage);
+    app.stage.height = 256;
+
+
+    //3种方法可以从纹理图集框架制作精灵    
+
+    let avatar_1, avatar_2, avatar_3, id;
+    // 1.直接访问纹理缓存
+    let avatar_1Texture = TextureCache['avatar_1.png'];
+    avatar_1 = new Sprite(avatar_1Texture);
+    avatar_1.width = 120;
+    avatar_1.height = 80;
+    app.stage.addChild(avatar_1);
+
+    //2.通过加载程序的资源访问纹理
+    avatar_2 = new Sprite(resources["./asset/img/main.json"].textures["avatar_2.png"]);
+    avatar_2.x = 120;
+    avatar_2.width = 120;
+    avatar_2.height = 80;
+
+    //使用资源管理器垂直居中
+    console.info('xxxxxxx', app.stage.height);
+    avatar_2.y = app.stage.height / 2 - avatar_2.height / 2;
+    app.stage.addChild(avatar_2);
+
+    //3.为所有纹理图集创建一个名为"id"的可选别名
+    //帧 id 纹理
+    id = resources["./asset/img/main.json"].textures;
+
+    // 使用别名制作
+    avatar_3 = new Sprite(id["avatar_3.png"]);
+    avatar_3.width = 120;
+    avatar_3.height = 80;
+    //Position the avatar_3 next to the right edge of the canvas  
+    avatar_3.x = app.stage.width - avatar_3.width;
+    avatar_3.y = app.stage.height;
+    app.stage.addChild(avatar_3);
 
 }
